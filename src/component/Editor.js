16d3b96602b8133e455f8e1530c3614d1361237f
webgori <webgori@gmail.com>
2023-06-08 01:33:58 +0900

    파일 수정

--- src/component/Editor.js
+++ src/component/Editor.js
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
 import Grid from '@mui/material/Grid';
 import MDEditor from '@uiw/react-md-editor';
 import axios from "axios";
 
 function Editor() {
     const [value, setValue] = React.useState("**Hello world!!!**");
-
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         let accessToken = localStorage.getItem("accessToken");
@@ -20,67 +17,57 @@ function Editor() {
         const searchParams = new URLSearchParams(document.location.search)
         let code = searchParams.get("code");
 
-        console.log(accessToken);
-
         let clientId = localStorage.getItem("clientId");
 
         if (code == null) {
             window.location.replace("https://github.com/login/oauth/authorize?client_id=" + clientId + "&scope=repo,user");
+            return;
         }
 
-        console.log("aa")
-        getAccessToken(code)
+        async function setAccessToken() {
+            let accessToken = await getAccessToken(code);
 
-        if (accessToken == null) {
-            throw new TypeError("accessToken is null")
+            if (accessToken == null) {
+                throw new TypeError("accessToken is null")
+            }
+
+            localStorage.setItem("accessToken", accessToken);
         }
 
-        localStorage.setItem("accessToken", accessToken);
-    });
+        setAccessToken();
+    }, []);
 
     async function getAccessToken(code) {
         let clientId = localStorage.getItem("clientId");
         let clientSecret = localStorage.getItem("clientSecret");
 
-        /*const aa = await fetch("https://github.com/login/oauth/access_token", {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: {
-                    client_id: clientId,
-                    client_secret: clientSecret,
-                    code: code
-                }
-            });*/
-
-        /*const response = await fetch(`https://github.com/login/oauth/access_token`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                client_id: clientId,
-                client_secret: clientSecret,
-                code: code
-            })
-        });
-
-        console.log(response);*/
-
-        const axiosResponse = await axios.post('http://localhost:8000/oauth/github/access-token', JSON.stringify({
+        let response = await axios.post('/oauth/github/access-token', JSON.stringify({
             client_id: clientId,
             client_secret: clientSecret,
             code: code
         }), {
             headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
+                'Content-Type': 'application/json'
             },
         });
 
-        console.log(axiosResponse);
+        return response.data["accessToken"];
+    }
+
+    async function getRepositories(code) {
+        let accessToken = localStorage.getItem("accessToken", accessToken);
+
+        /*let response = await axios.post('/oauth/github/access-token', JSON.stringify({
+            client_id: clientId,
+            client_secret: clientSecret,
+            code: code
+        }), {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+
+        return response.data["accessToken"];*/
     }
 
     return (
